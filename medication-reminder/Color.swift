12e43c5a5c891123e6b5ae489baa5d84c01836cf
marcsnts <msanto03@mail.uoguelph.ca>
2017-03-21 21:18:35 -0400

    Added constraints and made nav bar nice

---
+++ medication-reminder/Color.swift
@@ -0,0 +1,16 @@
+//
+//  Color.swift
+//  medication-reminder
+//
+//  Created by Marc Santos on 2017-03-21.
+//  Copyright © 2017 Vikas Gandhi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Color {
+    
+    static let MavencareBlue = UIColor(red: 52/255, green: 170/255, blue: 217/255, alpha: 1)
+
+}
--- medication-reminder/MedicationReminderViewController.swift
+++ medication-reminder/MedicationReminderViewController.swift
@@ -7,8 +7,8 @@
 //
 
 import UIKit
-import SwiftyJSON
 import FSCalendar
+import SnapKit
 
 class MedicationReminderViewController: UIViewController, FSCalendarDataSource, FSCalendarDelegate {
 
@@ -17,11 +17,50 @@ class MedicationReminderViewController: UIViewController, FSCalendarDataSource, 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupNavBar()
         self.calendar.select(Date())
-        
         self.calendar.scope = .week
-
+        self.calendar.delegate = self
+        self.calendar.dataSource = self
+        self.tableView.isHidden = false
+        
+        setupConstraints()
+        
+    }
+    
+    private func setupNavBar() {
+        self.title = "Medication Reminder"
+        self.navigationController?.navigationBar.barTintColor = Color.MavencareBlue
+        self.navigationController?.navigationBar.titleTextAttributes = [
+            NSForegroundColorAttributeName : UIColor.white
+        ]
+    }
+    
+    private func setupConstraints() {
+        self.calendar.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(self.topLayoutGuide.snp.bottom)
+            make.height.equalTo(200)
+            make.width.equalToSuperview()
+            make.centerX.equalToSuperview()
+        }
+        
+        self.tableView.snp.makeConstraints { (make) -> Void in
+            make.width.equalToSuperview()
+            make.top.equalTo(self.calendar.snp.bottom)
+            make.bottom.equalToSuperview()
+            make.centerX.equalToSuperview()
+        }
+    }
+    
+    
+    
+    
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        calendar.snp.updateConstraints { (make) in
+            make.height.equalTo(bounds.height)
+            // Do other updates
+        }
+        self.view.layoutIfNeeded()
     }
 
 
